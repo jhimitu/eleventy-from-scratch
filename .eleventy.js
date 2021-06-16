c70c830489d153a477bf63fc728089735bc1e5e2
Jhia Turner <jhimitu@gmail.com>
2021-06-15 18:55:31 -0700

    install eleventy

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+// this is the eleventy config file, the heartbeat of your website ❤️, the command center 🕹...
+module.exports = config => {
+    return {
+        dir: {
+            input: 'src',
+            output: 'dist'
+        }
+    };
+};
